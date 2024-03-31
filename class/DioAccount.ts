582c2840978acf0c997a2632fff19685077c2f6c
Willer Lucoles <132530244+WillerLucoles@users.noreply.github.com>
2024-03-31 19:09:03 -0300

    Add files via upload

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,42 +1,50 @@
 export abstract class DioAccount {
-  private name: string
-  private readonly accountNumber: number
-  balance: number = 0
-  private status: boolean = true
+  private name: string;
+  private readonly accountNumber: number;
+  protected balance: number = 0;
+  private status: boolean = true;
 
-  constructor(name: string, accountNumber: number){
-    this.name = name
-    this.accountNumber = accountNumber
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
   }
 
-  setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
+  setName(name: string): void {
+    this.name = name;
+    console.log("Nome alterado com sucesso!");
   }
 
-  getName = (): string => {
-    return this.name
+  getName(): string {
+    return this.name;
   }
 
-  deposit = (): void => {
-    if(this.validateStatus()){
-      console.log('Voce depositou')
+  deposit(amount: number): void {
+    if (this.validateStatus()) {
+      this.balance += amount;
+      console.log(`Depósito de ${amount} realizado com sucesso!`);
     }
   }
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
+  withdraw(amount: number): void {
+    if (this.validateStatus()) {
+      if (this.balance >= amount) {
+        this.balance -= amount;
+        console.log(`Saque de R$${amount.toFixed(2)} realizado com sucesso!`);
+      } else {
+        console.log("Saldo insuficiente para realizar o saque");
+      }
+    }
   }
 
-  getBalance = (): void => {
-    console.log(this.balance)
+  getBalance(): void {
+    console.log(`Saldo atual: R$${this.balance.toFixed(2)}`);
   }
 
-  private validateStatus = (): boolean => {
+  private validateStatus(): boolean {
     if (this.status) {
-      return this.status
+      return true;
     }
 
-    throw new Error('Conta inválida')
+    throw new Error("Conta inválida");
   }
 }
